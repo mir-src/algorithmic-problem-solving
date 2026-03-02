09108338a3c688c5ad273bcb975f714c6d3b2ac2
mir-src <264200162+mir-src@users.noreply.github.com>
2026-03-02 08:56:42 +0200

    Fixed: Binary Search

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -14,7 +14,6 @@ class Solution:
     def search(self, nums: list[int], target: int) -> int:
         left = 0
         right = len(nums) - 1
-        mid = (left + right) // 2
         while left <= right:
             mid = (left + right) // 2
             if nums[mid] == target:
