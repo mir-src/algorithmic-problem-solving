070659065ab347d9e1ebcdc905bc8f76d5281607
mir-src <264200162+mir-src@users.noreply.github.com>
2026-03-02 08:49:07 +0200

    Initial commit: Added Binary Search and README

---
+++ README.md
@@ -0,0 +1,16 @@
+# Algorithmic Problem Solving 🚀
+
+My personal collection of LeetCode solutions, focused on deep understanding and Big O efficiency. 
+
+## 🎯 The Goal
+I treat every problem as an engineering task. Each solution includes:
+* **Deep Thinking:** A written "Approach" before the code.
+* **Analysis:** Strict Time and Space complexity evaluation.
+* **Testing:** Local test cases to ensure reliability.
+
+## 📊 Progress
+- **Total Solved:** 1
+
+## 🛠️ How to run
+1. Clone the repo: `gh repo clone mir-src/algorithmic-problem-solving`
+2. Run any file: `python3 category/problem_name.py`
---
+++ binary_search/binary_search.py
@@ -0,0 +1,40 @@
+"""
+Problem: 704. Binary Search
+Difficulty: Easy
+URL: https://leetcode.com/problems/binary-search/
+
+My Approach:
+Split the indexes in two (not the list itself), compare the middle element with the target, if they are equal we return the index of the middle element, we make the while loop left <= right (<= because we want to take into account an array with only 1 element), I return -1 (no element is equal to target inside the list).
+
+Complexity Analysis:
+- Time Complexity: O(log n) -> Because we devide the search area in half each time we go through the list
+- Space Complexity: O(1) -> We only use a constant amount of extra space for pointers.
+"""
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        mid = (left + right) // 2
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif len(nums) == 1 and ([nums[0] != target]):
+                return -1
+            elif target < nums[mid]:
+                right = mid - 1
+            elif target >= nums[mid]:
+                left = mid + 1
+        return -1
+
+if __name__ == "__main__":
+    solver = Solution()
+
+    # Test 1: Element exists
+    result1 = solver.search([-1,0,3,5,9,12], 9)
+    print(f"Test 1: {result1} | Expected: 4")
+
+    # Test 2: Element doesn't exist
+    result2 = solver.search([-1,0,3,5,9,12], 2)
+    print(f"Test 2: {result2} | Expected: -1")
+
